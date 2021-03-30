4b535855de381a4e124e44044ea4c24a06794813
Real96 <chicco.gaeta@gmail.com>
2021-03-30 18:50:55 +0200

    Initial commit

---
+++ RS-Box Shift Calc/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace RS_Box_Shift_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this._MaskedTextBox4 = new RS_Box_Shift_Calc.Controls._MaskedTextBox(this.components);
+            this._MaskedTextBox3 = new RS_Box_Shift_Calc.Controls._MaskedTextBox(this.components);
+            this._MaskedTextBox2 = new RS_Box_Shift_Calc.Controls._MaskedTextBox(this.components);
+            this._MaskedTextBox1 = new RS_Box_Shift_Calc.Controls._MaskedTextBox(this.components);
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(131, 148);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 41;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(52, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 40;
+            this.label9.Text = "Current Frame:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 13);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Target Frame:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Groudon/Kyogre",
+            "Rayquaza",
+            "Regirock",
+            "Registeel",
+            "Regice",
+            "Latias (Southern Island)",
+            "Latios (Southern Island)"});
+            this.comboBox2.Location = new System.Drawing.Point(137, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(137, 268);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(121, 20);
+            this.textBox6.TabIndex = 33;
+            this.textBox6.TabStop = false;
+            this.textBox6.DoubleClick += new System.EventHandler(this.TextBox6_DoubleClick);
+            this.textBox6.Enter += new System.EventHandler(this.TextBox6_Enter);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 13);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Dolphin Target Frame:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Pokémon:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "JPN",
+            "USA",
+            "PAL"});
+            this.comboBox1.Location = new System.Drawing.Point(137, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Dolphin Visual Frame:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Current Seed:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Initial Seed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Current Seed Location:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Region:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 21);
+            this.button2.TabIndex = 42;
+            this.button2.Text = "About";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // _MaskedTextBox4
+            // 
+            this._MaskedTextBox4.Location = new System.Drawing.Point(137, 171);
+            this._MaskedTextBox4.Mask = "9999999999";
+            this._MaskedTextBox4.Name = "_MaskedTextBox4";
+            this._MaskedTextBox4.Size = new System.Drawing.Size(121, 20);
+            this._MaskedTextBox4.TabIndex = 5;
+            this._MaskedTextBox4.Tag = "";
+            // 
+            // _MaskedTextBox3
+            // 
+            this._MaskedTextBox3.Location = new System.Drawing.Point(137, 197);
+            this._MaskedTextBox3.Mask = "9999999999";
+            this._MaskedTextBox3.Name = "_MaskedTextBox3";
+            this._MaskedTextBox3.Size = new System.Drawing.Size(121, 20);
+            this._MaskedTextBox3.TabIndex = 6;
+            this._MaskedTextBox3.Tag = "frame";
+            // 
+            // _MaskedTextBox2
+            // 
+            this._MaskedTextBox2.Location = new System.Drawing.Point(137, 119);
+            this._MaskedTextBox2.Mask = "AAAAAAAA";
+            this._MaskedTextBox2.Name = "_MaskedTextBox2";
+            this._MaskedTextBox2.Size = new System.Drawing.Size(121, 20);
+            this._MaskedTextBox2.TabIndex = 4;
+            // 
+            // _MaskedTextBox1
+            // 
+            this._MaskedTextBox1.Location = new System.Drawing.Point(137, 93);
+            this._MaskedTextBox1.Mask = "AAAA";
+            this._MaskedTextBox1.Name = "_MaskedTextBox1";
+            this._MaskedTextBox1.Size = new System.Drawing.Size(121, 20);
+            this._MaskedTextBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 298);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this._MaskedTextBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this._MaskedTextBox3);
+            this.Controls.Add(this._MaskedTextBox2);
+            this.Controls.Add(this._MaskedTextBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RS-Box Shift Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private Controls._MaskedTextBox _MaskedTextBox4;
+        private System.Windows.Forms.Label label8;
+        private Controls._MaskedTextBox _MaskedTextBox3;
+        private Controls._MaskedTextBox _MaskedTextBox2;
+        private Controls._MaskedTextBox _MaskedTextBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ RS-Box Shift Calc/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System.Windows.Forms;
+using System.Globalization;
+
+namespace RS_Box_Shift_Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            if (_MaskedTextBox1.Text != "" && _MaskedTextBox2.Text != "" && _MaskedTextBox3.Text != "" && _MaskedTextBox4.Text != "")
+            {
+                uint initialSeed = uint.Parse(_MaskedTextBox1.Text, NumberStyles.HexNumber);
+                string currentSeed = _MaskedTextBox2.Text;
+
+                if (currentSeed.Length < 8)
+                {
+                    MessageBox.Show("The inserted Current Seed is too short", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string currentSeedInverted = "";
+
+                    for (int i = 6; i >= 0; i -= 2)
+                    {
+                        currentSeedInverted += currentSeed.Substring(i, 2);
+                    }
+                    uint currentSeedFinal = uint.Parse(currentSeedInverted, NumberStyles.HexNumber);
+                    uint dolphinFrame = uint.Parse(_MaskedTextBox4.Text);
+                    uint vbaTarget = uint.Parse(_MaskedTextBox3.Text);
+                    uint frame = 1;
+                    uint pokémonOffset;
+
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        pokémonOffset = 654;
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+                        pokémonOffset = 2;
+                    }
+                    else if (comboBox2.SelectedIndex == 5)
+                    {
+                        pokémonOffset = 404;
+                    }
+                    else if (comboBox2.SelectedIndex == 6)
+                    {
+                        pokémonOffset = 431;
+                    }
+                    else
+                    { pokémonOffset = 56; }
+
+                    if (vbaTarget <= pokémonOffset)
+                    {
+                        MessageBox.Show("Target Frame is lower than the Offset", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        while (initialSeed != currentSeedFinal)
+                        {
+                            uint result = 0x41C64E6D * initialSeed + 0x00006073;
+                            frame++;
+                            initialSeed = result;
+                        }
+                        label10.Text = frame.ToString();
+
+                        if ((vbaTarget - frame) < pokémonOffset)
+                        {
+                            MessageBox.Show("Target Frame - Current Frame is lower than the Offset", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        { textBox6.Text = (vbaTarget - frame + dolphinFrame - pokémonOffset).ToString(); }
+                    }
+                }
+            }
+            else
+            { MessageBox.Show("Please fill all the fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                textBox1.Text = "80B1E9F8";
+            }
+        }
+
+        private void TextBox6_Enter(object sender, System.EventArgs e)
+        {
+            label1.Focus();
+        }
+
+        private void Button2_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("New Order Of Breeding © \nOriginal concept and idea by _Dragon & Drakero \nAddresses found by N.O.O.B. Team \nDeveloped by Real96 & Signum21", "About", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void TextBox6_DoubleClick(object sender, System.EventArgs e)
+        {
+            textBox6.Text = "";
+        }
+    }
+}
